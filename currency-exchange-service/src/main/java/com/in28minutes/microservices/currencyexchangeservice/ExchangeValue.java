eaa675b955afd52d0e72faebc0b2c440bc40ec00
egiraldo <egiraldo@localhost>
2018-03-13 11:45:57 -0500

    add jpa for handling DB

--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/ExchangeValue.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/ExchangeValue.java
@@ -2,24 +2,35 @@ package com.in28minutes.microservices.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class ExchangeValue {
 
+	@Id
 	private Long id;
+	
+	@Column(name="currency_from")
 	private String from;
+	
+	@Column(name="currency_to")
 	private String to;
-	private BigDecimal conversionMiltiple;
+	
+	private BigDecimal conversionMultiple;
 	private int port;
 
 	public ExchangeValue() {
 
 	}
 
-	public ExchangeValue(Long id, String from, String to, BigDecimal conversionMiltiple) {
+	public ExchangeValue(Long id, String from, String to, BigDecimal conversionMultiple) {
 		super();
 		this.id = id;
 		this.from = from;
 		this.to = to;
-		this.conversionMiltiple = conversionMiltiple;
+		this.conversionMultiple = conversionMultiple;
 	}
 
 	public Long getId() {
@@ -46,8 +57,12 @@ public class ExchangeValue {
 		this.to = to;
 	}
 
-	public BigDecimal getConversionMiltiple() {
-		return conversionMiltiple;
+	public BigDecimal getConversionMultiple() {
+		return conversionMultiple;
+	}
+	
+	public void setConversionMultiple(BigDecimal conversionMultiple) {
+		this.conversionMultiple = conversionMultiple;
 	}
 	
 	public int getPort() {
@@ -56,10 +71,6 @@ public class ExchangeValue {
 
 	public void setPort(int port) {
 		this.port = port;
-	}
-
-	public void setConversionMiltiple(BigDecimal conversionMiltiple) {
-		this.conversionMiltiple = conversionMiltiple;
 	}
 
 }
---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+Insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10001,'USD','COP', 2700,0);
+Insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10002,'EURO','COP', 3200,0);
+Insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10003,'USD','EURO', 2,0);
