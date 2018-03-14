652785078da4c9e87a1bb6576d2e90488fe58208
Elkin Giraldo <elkin.giraldo@telintel.net>
2018-03-14 11:26:22 -0500

    add log

--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.in28minutes.microservices.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,17 +13,22 @@ public class CurrencyExchangeController {
 
 	@Autowired
 	private Environment environment;
-	
+
 	@Autowired
 	private ExchangeValueRepository exchangeValueRepository;
 
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
 	@GetMapping("currency-exchange/from/{from}/to/{to}")
 	public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
-		
+
 		ExchangeValue exchangeValue = exchangeValueRepository.findByFromAndTo(from, to);
-		
-		// ExchangeValue exchangeValue = new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
+
+		// ExchangeValue exchangeValue = new ExchangeValue(1000L, from, to,
+		// BigDecimal.valueOf(65));
 		exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+
+		logger.info("{}", exchangeValue);
 
 		return exchangeValue;
 	}
